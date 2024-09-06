80b5378cfa0709dc77db50b603e7a2a648fa2c82
llindsa1 <lindsayuchiha@icloud.com>
2024-09-06 16:08:27 -0400

    added instructions

--- 308.1.1 Lab.js
+++ 308.1.1 Lab.js
@@ -1,3 +1,35 @@
+/* 
+Part One: Math Problems
+Implement the following:
+Check if all numbers are divisible by 5. Cache the result in a variable.
+Check if the first number is larger than the last. Cache the result in a variable.
+Accomplish the following arithmetic chain:
+Subtract the first number from the second number.
+Multiply the result by the third number.
+Find the remainder of dividing the result by the fourth number.
+Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+
+
+/*
+ Part Two: Practical Math
+ You are planning a cross-country road trip!
+The distance of the trip, in total, is 1,500 miles.
+Your car’s fuel efficiency is as follows:
+At 55 miles per hour, you get 30 miles per gallon.
+At 60 miles per hour, you get 28 miles per gallon.
+At 75 miles per hour, you get 23 miles per gallon.
+You have a fuel budget of $175.
+The average cost of fuel is $3 per gallon.
+Set up a program to answer the following questions:
+How many gallons of fuel will you need for the entire trip?
+Will your budget be enough to cover the fuel expense?
+How long will the trip take, in hours?
+Compare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?
+Log the results of your calculations using string concatenation or template literals.
+
+//Part Three: Future Exploration
+
+
 // The initial numbers that must be verified.
 const n1 = 10;
 const n2 = 15;
